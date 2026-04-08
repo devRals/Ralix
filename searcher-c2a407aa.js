08f07408cb8b9e3b4817434850079c5fe0bce4d2
devRals <devRals@users.noreply.github.com>
2026-04-08 12:57:47 +0000

    deploy: c4cf9216f73d901a925e3f1abbcc201cb185f43f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3a02f8d0.js',
+                path_to_root + 'searchindex-b898fed7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
